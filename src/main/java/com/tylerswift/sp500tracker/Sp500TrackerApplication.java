c7eef762a314c2cb5b19cc83cea921335fe70c02
Tyler Rodriguez <tylerrodriguez@tylers-mbp.attlocal.net>
2020-12-17 21:52:51 -0600

    Setup Project

---
+++ src/main/java/com/tylerswift/sp500tracker/Sp500TrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tylerswift.sp500tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp500TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp500TrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tylerswift/sp500tracker/Sp500TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylerswift.sp500tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp500TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
